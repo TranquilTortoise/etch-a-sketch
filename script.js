ea3a37d1250cd8b2ebcca8a3d3383df88ada8df7
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-31 07:55:28 -1000

    fix grid; create buttons w/ limited functionality

--- script.js
+++ script.js
@@ -3,14 +3,33 @@ const container = document.querySelector(".container");
 
 // create grid
 for (let i = 0; i < GRID_SIZE; i++) {
+
+    // create rows
     const row = document.createElement("div");
     row.setAttribute("class", "row");
     container.appendChild(row);
     
     for (let j =0; j < GRID_SIZE; j++) {
+
+        // create squares
         const square = document.createElement("div");
-        square.setAttribute("class", "square");
-        square.setAttribute("style", `height: ${80 / GRID_SIZE}vh; width: ${80 / GRID_SIZE}vh;`);;
+        square.setAttribute("class", "square");       
         row.appendChild(square);
     }
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.setAttribute("style", "background-color: purple;");
+    })
+})
+
+const resetBtn = document.querySelector(".reset-btn");
+
+resetBtn.addEventListener('click', () => {
+    squares.forEach((square) => {
+        square.setAttribute("style", "background-color: '';");
+    })
+})
